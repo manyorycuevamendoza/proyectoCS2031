21e5bc15842efee0a4dd89b75b98f3e98ae026f2
Manyory Cueva Mendoza <91238621+manyorycuevamendoza@users.noreply.github.com>
2022-09-25 15:02:22 -0500

    update proyecto.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'postgresql://postgres:76591212@localhost:5432/blueprint'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
